ef9ba4c56ba90a8607bc24babe3da835c496a6d8
abdul <abdulrehman133782@gmail.com>
2022-12-03 14:49:14 +1100

    updated login page hyperlink to properly display correct page label

--- halalfind-frontend/src/App.vue
+++ halalfind-frontend/src/App.vue
@@ -9,12 +9,12 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it???!\n hellossss" />
-
+      <test msg="testing123"/>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/login">About</RouterLink>
-
+        <RouterLink to="/login">Login</RouterLink>
+        <RouterLink to="/test">Test</RouterLink>
       </nav>
     </div>
   </header>
--- halalfind-frontend/src/router/index.ts
+++ halalfind-frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import LoginView from "../views/LoginView.vue";
+import Test from "../views/Test.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: LoginView,
     },
+    {
+      path: "/test",
+      name: "test",
+      component: Test,
+    }
   ],
 });
 
